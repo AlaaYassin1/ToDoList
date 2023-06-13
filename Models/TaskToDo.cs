7a341a22873d5cc6d7f87fd66420d78c21f4b879
Alaa Yassin <106423566+AlaaYassin1@users.noreply.github.com>
2023-06-13 16:59:28 +0300

    Add files via upload

---
+++ Models/TaskToDo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class TaskToDo
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public string description { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? Date { get; set; }
+
+        public int IsCompleted { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category? Category { get; set; }
+
+    }
+}
